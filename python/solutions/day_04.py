37854a47ca21311f7c36cd448a3cf7940792ac58
Katie Gardner <5213160+KatieLG@users.noreply.github.com>
2025-12-04 06:33:29 +0000

    day04: cleanup python solution a bit

--- python/solutions/day_04.py
+++ python/solutions/day_04.py
@@ -8,47 +8,37 @@ class Day04(AOCSolution):
     }
 
     def __post_init__(self) -> None:
-        self.width = len(self.parsed_data[0])
-        self.height = len(self.parsed_data)
-        self.grid = self.parsed_data
+        self.grid = [[int(x == "@") for x in row] for row in self.data.splitlines()]
+        self.height = len(self.grid)
+        self.width = len(self.grid[-1])
 
-    @property
-    def parsed_data(self) -> list[list[int]]:
-        """Parse and return the input data."""
-        return [[int(x == "@") for x in row] for row in self.data.splitlines()]
-
-    def count_nbrs(self, r: int, c: int) -> int:
+    def count_nbr_rolls(self, r: int, c: int) -> int:
         total = 0
-        nbrs = [(r + i, c + j) for i in range(-1, 2) for j in range(-1, 2) if not i==j==0]
+        nbrs = [(r + i, c + j) for i in range(-1, 2) for j in range(-1, 2) if not i == j == 0]
         for y, x in nbrs:
             if y in range(self.height) and x in range(self.width):
                 total += self.grid[y][x]
         return total
 
     def removable(self) -> list[tuple[int, int]]:
-        valid = []
-        for r in range(self.height):
-            for c in range(self.width):
-                if self.count_nbrs(r, c) < 4 and self.grid[r][c]:
-                    valid.append((r, c))
-
-        return valid
+        return [
+            (r, c)
+            for r in range(self.height)
+            for c in range(self.width)
+            if self.grid[r][c] and self.count_nbr_rolls(r, c) < 4
+        ]
 
     def part_one(self) -> int:
-        """Solve part one."""
+        """Count removable rolls"""
         return len(self.removable())
 
     def part_two(self) -> int:
-        """Solve part two."""
+        """Keep removing removable rolls until its no longer possible and count total removed"""
         total = 0
-        while True:
-            removable = self.removable()
-            if len(removable):
-                for r, c in removable:
-                    self.grid[r][c] = 0
-                total += len(removable)
-            else:
-                break
+        while removable := self.removable():
+            for r, c in removable:
+                self.grid[r][c] = 0
+            total += len(removable)
         return total
 
 
