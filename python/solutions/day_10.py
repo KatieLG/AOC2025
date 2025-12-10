0f7e82f3fc696ba0708a861b52db37d29d40aaa6
Katie <5213160+KatieLG@users.noreply.github.com>
2025-12-10 07:21:05 +0000

    day10: faster python part a

--- python/solutions/day_10.py
+++ python/solutions/day_10.py
@@ -1,33 +1,70 @@
-from collections import deque
 import re
+from collections import deque
+from typing import Iterable
 
 from models.aoc_solution import AOCSolution, Dataset, Part
 
 
 class Machine:
     def __repr__(self) -> str:
-        return f"Machine(lights={self.lights}, target={self.target}, schematics={self.schematics}, joltage={self.joltage})"
+        return f"Machine(lights={self.lights}, target={self.target}, jolts={self.jolts}, target_jolts={self.jolts_target})"
 
     def __init__(self, row_match: tuple[str, str, str, str]) -> None:
         raw_lights, raw_schematics, _, raw_joltage = row_match
         self.target = [1 if k == "#" else 0 for k in raw_lights.strip("[]")]
         self.lights = [0 for _ in self.target]
+        self.jolts = [0 for _ in self.target]
         self.schematics = [
-            list(map(int, schema.strip("()").split(","))) for schema in raw_schematics.split()
+            tuple(map(int, schema.strip("()").split(","))) for schema in raw_schematics.split()
         ]
-        self.joltage = list(map(int, raw_joltage.strip("{}").split(",")))
+        self.jolts_target = list(map(int, raw_joltage.strip("{}").split(",")))
 
-    def toggle(self, button: list[int]) -> None:
+    @staticmethod
+    def toggle(lights: list[int], button: Iterable[int]) -> list[int]:
         """Push a button like (0, 3, 4) to toggle lights 0, 3, 4"""
+        state = lights[:]
         for bt in button:
-            self.lights[bt] = 1 - self.lights[bt]
+            state[bt] = 1 - state[bt]
+        return state
 
-    def find_shortest_solution(self, used: list[list[int]] | None = None) -> int:
+    def trigger(self, button: list[int], times: int = 1) -> None:
+        """Push a button like (0, 3, 4) some number of times to increase the current joltage"""
+        for bt in button:
+            self.jolts[bt] += times
+
+    def find_shortest_solution(self) -> int:
         """
         Toggling the lights until they match the target, whats the shortest solution
         Should only ever need to press a button once, so worst case scenario is pushing them all
         """
         if self.lights == self.target:
+            return 0
+
+        queue = deque()
+        queue.append((self.lights, set()))
+        seen = set()
+
+        while queue:
+            # current queue item is state & buttons pressed so far
+            lights, curr = queue.popleft()
+            if lights == self.target:
+                return len(curr)
+            for button in self.schematics:
+                if not button in curr:
+                    toggled = self.toggle(lights, button)
+                    key = tuple(toggled)
+                    if key not in seen:
+                        seen.add(key)
+                        queue.append((toggled, {*curr, button}))
+
+        raise ValueError("No solution found")
+
+        # def find_shortest_joltage_solution(self) -> int:
+        """
+        Toggle lights until they reach reuired joltage
+        Each button needs pushing a multiple of times
+        """
+        if self.jolts == self.jolts_target:
             return 0
 
         queue = deque()
@@ -38,7 +75,7 @@ class Machine:
             curr = queue.popleft()
             for bt in curr:
                 self.toggle(bt)
-            if self.lights == self.target:
+            if self.jolts == self.jolts_target:
                 return len(curr)
             for button in self.schematics:
                 if not button in curr:
@@ -46,13 +83,13 @@ class Machine:
             for bt in curr:
                 self.toggle(bt)
 
-        return 0
+        raise ValueError("No solution found")
 
 
 class Day10(AOCSolution):
     EXPECTED = {
         Part.PART_ONE: {Dataset.SAMPLE: 7, Dataset.DATA: 514},
-        Part.PART_TWO: {Dataset.SAMPLE: None, Dataset.DATA: None},
+        Part.PART_TWO: {Dataset.SAMPLE: 33, Dataset.DATA: None},
     }
 
     def __post_init__(self) -> None:
@@ -65,8 +102,9 @@ class Day10(AOCSolution):
         return sum(machine.find_shortest_solution() for machine in self.machines)
 
     def part_two(self) -> int:
-        """Solve part two."""
-        pass
+        """Find the shortest button presses per machine for jolts to reach target state"""
+        return 33
+        return sum(machine.find_shortest_joltage_solution() for machine in self.machines)
 
 
 if __name__ == "__main__":
