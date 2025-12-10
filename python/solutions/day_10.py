c1163637d28daa4e30dc381461f22cb4eb0e35f4
Katie <5213160+KatieLG@users.noreply.github.com>
2025-12-10 06:30:56 +0000

    day10: python part a binary

--- python/solutions/day_10.py
+++ python/solutions/day_10.py
@@ -1,28 +1,33 @@
-from collections import deque
 import re
+from collections import deque
 
 from models.aoc_solution import AOCSolution, Dataset, Part
 
 
 class Machine:
-    def __repr__(self) -> str:
-        return f"Machine(lights={self.lights}, target={self.target}, schematics={self.schematics}, joltage={self.joltage})"
 
     def __init__(self, row_match: tuple[str, str, str, str]) -> None:
         raw_lights, raw_schematics, _, raw_joltage = row_match
-        self.target = [1 if k == "#" else 0 for k in raw_lights.strip("[]")]
-        self.lights = [0 for _ in self.target]
-        self.schematics = [
-            list(map(int, schema.strip("()").split(","))) for schema in raw_schematics.split()
-        ]
+        self.target = int(raw_lights.strip("[],").replace(".", "0").replace("#", "1")[::-1], 2)
+        self.lights = 0
+        self.schematics = [self.parse_raw_schematic(schema) for schema in raw_schematics.split()]
+        self.width = len(bin(max(self.schematics))) - 2
         self.joltage = list(map(int, raw_joltage.strip("{}").split(",")))
 
-    def toggle(self, button: list[int]) -> None:
-        """Push a button like (0, 3, 4) to toggle lights 0, 3, 4"""
-        for bt in button:
-            self.lights[bt] = 1 - self.lights[bt]
+    def __repr__(self) -> str:
+        w = self.width
+        pretty_schematics = [f"{k:0{w}b}" for k in self.schematics]
+        return f"Machine(lights={self.lights:0{w}b}, target={self.target:0{w}b}, schematics={pretty_schematics}, joltage={self.joltage})"
 
-    def find_shortest_solution(self, used: list[list[int]] | None = None) -> int:
+    @staticmethod
+    def parse_raw_schematic(schematic: str) -> int:
+        """Parse (0, 3, 4) into a binary toggle number"""
+        mask = 0
+        for number in map(int, schematic.strip("()").split(",")):
+            mask |= 1 << number
+        return mask
+
+    def find_shortest_solution(self) -> int:
         """
         Toggling the lights until they match the target, whats the shortest solution
         Should only ever need to press a button once, so worst case scenario is pushing them all
@@ -37,22 +42,22 @@ class Machine:
             # current queue item is buttons pressed so far
             curr = queue.popleft()
             for bt in curr:
-                self.toggle(bt)
+                self.lights ^= bt
             if self.lights == self.target:
                 return len(curr)
             for button in self.schematics:
                 if not button in curr:
                     queue.append([*curr, button])
             for bt in curr:
-                self.toggle(bt)
+                self.lights ^= bt
 
-        return 0
+        raise ValueError("No solution to light toggles")
 
 
 class Day10(AOCSolution):
     EXPECTED = {
         Part.PART_ONE: {Dataset.SAMPLE: 7, Dataset.DATA: 514},
-        Part.PART_TWO: {Dataset.SAMPLE: None, Dataset.DATA: None},
+        Part.PART_TWO: {Dataset.SAMPLE: 33, Dataset.DATA: None},
     }
 
     def __post_init__(self) -> None:
