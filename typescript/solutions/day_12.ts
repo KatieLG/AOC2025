5a02bd405d2876b9651ff1136bb08a497a2b57a8
Katie <5213160+KatieLG@users.noreply.github.com>
2025-12-20 18:32:55 +0000

    day12: simplify typescript

--- typescript/solutions/day_12.ts
+++ typescript/solutions/day_12.ts
@@ -4,26 +4,18 @@ interface Requirement {
   counts: number[];
 }
 
-const parse = (input: string): [number[][][], Requirement[]] => {
+const parse = (input: string): Requirement[] => {
   const groups = input.split("\n\n");
   const reqText = groups.pop()!;
-  const requirements = reqText.split("\n").map((line) => {
+  return reqText.split("\n").map((line) => {
     const [w, h] = line.split(":")[0].split("x").map(Number);
     const rest = line.split(" ").slice(1).map(Number);
     return { width: w, height: h, counts: rest };
   });
-  const shapes = groups.map((group) =>
-    group
-      .trim()
-      .split("\n")
-      .slice(1)
-      .map((line) => line.split("").map((char) => (char === "#" ? 1 : 0))),
-  );
-  return [shapes, requirements];
 };
 
 export const part1 = (input: string): number => {
-  const [, requirements] = parse(input);
+  const requirements = parse(input);
   let valid = 0;
   for (const { width, height, counts } of requirements) {
     const threes = ((width / 3) | 0) * ((height / 3) | 0);
